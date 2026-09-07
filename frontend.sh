5b6b8feaf1c3568bb9cd6fd8f9393ee4f3a2b1f5
rakesh <rakeshgautam602@gmail.com>
2026-09-07 22:01:35 +0530

    actions

--- frontend.sh
+++ frontend.sh
@@ -18,10 +18,10 @@ fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-      echo -e "$2: $R FAILURE $N" &>> $LOG_FILE
+      echo -e "$2: $R FAILURE $N" | tee -a $LOG_FILE
       exit 1
     else
-      echo -e "$2: $G Success $N" &>> $LOG_FILE
+      echo -e "$2: $G Success $N" | tee -a $LOG_FILE
     fi
 }
 
