8b68bae6827575c9a2b006e755085353af72cedf
rakesh <rakeshgautam602@gmail.com>
2026-09-07 16:43:31 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -8,7 +8,7 @@ do
     aws ec2 run-instances \
         --image-id $AMI_ID \
         --instance-type t3.micro \
-        --security-group-ids SG_ID \
+        --security-group-ids $SG_ID \
         --tag-specifications 'ResourceType=$instance,Tags=[{Key=Name,Value=$instance}]' \
         --query 'Instances[0].PrivateIpAddress' \
         --output text
