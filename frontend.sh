1ca5d0a2243359f3b9cb70a0fc44b4f0e535e82a
rakesh <rakeshgautam602@gmail.com>
2026-09-07 21:38:36 +0530

    actions

--- frontend.sh
+++ frontend.sh
@@ -18,28 +18,32 @@ fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-      echo -e "$2: $R FAILURE $N"
+      echo -e "$2: $R FAILURE $N" >> $LOG_FILE
       exit 1
     else
-      echo "$2: $R Success $N"
+      echo -e "$2: $R Success $N" >> $LOG_FILE
     fi
 }
 
 dnf module disable nginx -y
-dnf module enable nginx:1.24 -y
+dnf module enable nginx:1.24 -y >> $LOG_FILE
 VALIDATE $? "Enabling nginx"
 
-dnf install nginx -y
+dnf install nginx -y >> $LOG_FILE
 VALIDATE $? "Installing nginx"
+
+systemctl enable nginx >> $LOG_FILE
+systemctl start nginx 
+VALIDATE $? "Enabling and Starting nginx"
 
 rm -rf /usr/share/nginx/html/* 
 VALIDATE $? "Removing default content"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip >> $LOG_FILE
 VALIDATE $? "Downloading frontend code"
 
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip >> $LOG_FILE
 VALIDATE $? "Unzipping frontend code"
 
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
