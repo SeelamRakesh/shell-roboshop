b2c9d7e525a569f269621da98d56c5279540ba38
rakesh <rakeshgautam602@gmail.com>
2026-09-07 17:06:12 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -5,13 +5,14 @@ SG_ID="sg-0e17db6ab1dfaa76a"
 
 for instance in $@
 do
-    INSTANCE_ID=(aws ec2 run-instances \
+    INSTANCE_ID=$(aws ec2 run-instances \
             --image-id $AMI_ID \
             --instance-type t3.micro \
             --security-group-ids $SG_ID \
             --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
             --query 'Instances[0].InstanceId' \
             --output text)
+
     if [ $instance == "frontend" ]; then
      aws ec2 describe-instances \
         --instance-ids $INSTANCE_ID \
