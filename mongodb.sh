87388999dd3120477b05ce94a8af817e9bf64d40
rakesh <rakeshgautam602@gmail.com>
2026-09-07 19:32:36 +0530

    actions

--- mongodb.sh
+++ mongodb.sh
@@ -1,44 +1,27 @@
 #!/bin/bash
 
 USER_ID=$(id -u)
-LOG_FOLDER="/var/log/shell-roboshop"
-LOG_FILE="$LOG_FOLDER/$0.log"
+LOG_FOLDER="/var/log/$PWD"
+LOG_FOLDER="$LOG_FOLDER/$0.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-mkdir -p $LOG_FOLDER
-
 if [ $USER_ID -ne 0 ]; then
-   echo "Run this script as root user" | tee -a $LOG_FILE
-   exit 1
+ echo "Run the script as Root User"
+ exit 1
 fi
 
-VALIDATE(){
-    if [ $1 -ne 0 ]; then
-      echo -e "$2: $R FAILURE $N" | tee -a $LOG_FILE
-      exit 1
-    else
-      echo -e "$2: $G Success $N" | tee -a $LOG_FILE
-    fi
+VALIDATE() {
+  if [ $1 -ne 0 ]; then
+   echo -e "$2 $R Failure $N"
+  else 
+   echo -e "$2 $G Success $N"
+  fi
 }
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? "Copying mongo Repo"
+cp $PWD/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying Mongo repo"
 
-dnf install mongodb-org -y  &>>$LOG_FILE
-VALIDATE $? "Installing MongoDB"
-
-systemctl enable mongod &>>$LOG_FILE
-VALIDATE $? "Enabling Mongodb"
-
-systemctl start mongod
-VALIDATE $? "Starting Mongodb"
-
-sed -i s/127.0.0.1/0.0.0.0/g /etc/mongod.conf
-VALIDATE $? "Allowing remote connections"
-
-systemctl restart mongod
-VALIDATE $? "Restarted MongoDB"
 
